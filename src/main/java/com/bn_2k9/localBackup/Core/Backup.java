cda50437afcf72ba83101bc69812fc18bb915f19
BN_2K9 <bn2k9mc@gmail.com>
2025-02-08 17:16:28 +0100

    Fixed Everything It Fully Works Now! ;)

--- src/main/java/com/bn_2k9/localBackup/Core/Backup.java
+++ src/main/java/com/bn_2k9/localBackup/Core/Backup.java
@@ -2,31 +2,25 @@ package com.bn_2k9.localBackup.Core;
 
 import com.bn_2k9.localBackup.LocalBackup;
 import com.bn_2k9.localBackup.Utils.Logger;
-import org.apache.commons.io.FileExistsException;
 import org.apache.commons.io.FileUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.chrono.ChronoLocalDateTime;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
-import static org.apache.commons.io.FileUtils.copyDirectory;
-import static org.apache.commons.io.FileUtils.copyDirectoryToDirectory;
 
 public class Backup {
 
     public void InitBackupTimer() {
-
+        // This Checks Every Half Hour if An Backup Is Needed.
         Bukkit.getScheduler().scheduleSyncRepeatingTask(LocalBackup.getInstance(), () -> {
 
             LocalTime currentTime = LocalTime.now();
@@ -36,7 +30,7 @@ public class Backup {
             }
 
 
-        }, 20 * 6,20 * 600);
+        }, 20 * 6,20 * 1900);
 
     }
 
@@ -49,10 +43,12 @@ public class Backup {
             player.kickPlayer("Restarting For Backup!");
         }
 
+        // This Disables LuckPerms So When We Take An Backup It Doesn't Error Out.
         if (Bukkit.getServer().getPluginManager().getPlugin("luckperms") != null) {
             Bukkit.getServer().getPluginManager().getPlugin("luckperms").onDisable();
         }
 
+        // Unload All Worlds To Prevent Corruption.
         for (World world :Bukkit.getServer().getWorlds()) {
             world.save();
             Bukkit.getServer().unloadWorld(world, true);
@@ -68,6 +64,7 @@ public class Backup {
         } else {
             String[] names = new File(BackupFolder).list();
             if (names.length >= LocalBackup.getInstance().getConfig().getInt("MaxBackups")) {
+                Logger.LogInfo("Max Backups Reached Removing Oldest File.");
                 String FileToDelete = null;
                 for (String name : names) {
                     LocalDateTime currentDateTime = LocalDateTime.now();
@@ -81,9 +78,8 @@ public class Backup {
                     }
                 }
 
-                Bukkit.getLogger().info("Removing Backup");
 
-                FileToDelete.replace(":", "x");
+                FileToDelete.replace(":", "x").replace(".", "z");
                 File delete = new File(LocalBackup.getInstance().getDataFolder().getAbsolutePath() + "/Backups/" + FileToDelete);
                 try {
                     FileUtils.deleteDirectory(delete);
@@ -95,11 +91,11 @@ public class Backup {
         }
 
         String FolderPath = LocalBackup.getInstance().getDataFolder().getAbsolutePath().replace("LocalBackup", "").replace("plugins", "").replace(File.separator+File.separator, File.separator);
-        Bukkit.getLogger().info(FolderPath);
         File Folder = new File(FolderPath);
         String BackupFolderPath = LocalBackup.getInstance().getDataFolder().getAbsolutePath() + "/Backups/" + LocalDateTime.now().toString().replace(":", "x").replace(".", "z") + "/";
         File OutputFolder = new File(BackupFolderPath);
 
+        // Runs The Backup Async So it Doesn't Time Out The Main Thread.
         Bukkit.getScheduler().runTaskAsynchronously(LocalBackup.getInstance(), () -> {
 
             OutputFolder.mkdirs();
@@ -108,25 +104,31 @@ public class Backup {
                     @Override
                     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
 
+                        List<String> BlackListedNames = LocalBackup.getInstance().getConfig().getStringList("BlackListedNames");
+                        BlackListedNames.add("LocalBackup");
+                        BlackListedNames.add("session.lock");
+
                         if (!Files.isDirectory(file)) {
                             String path = file.toFile().getAbsolutePath().replace(Folder.getAbsolutePath(), "");
-                            Bukkit.getLogger().info(path);
 
-                            if (path.contains("session.lock") || path.contains("LocalBackup")) {
-                                Logger.LogInfo("Skipping Found Blacklisted File");
-                            } else {
-                                File newfile = new File(OutputFolder + File.separator + path);
-                                FileUtils.copyFile(file.toFile(), newfile);
+                            for (String BlackListedName: BlackListedNames) {
+                                if (path.contains(BlackListedName)) {
+                                    Logger.LogInfo("Skipping Found Blacklisted File");
+                                } else {
+                                    File newfile = new File(OutputFolder + File.separator + path);
+                                    FileUtils.copyFile(file.toFile(), newfile);
+                                }
                             }
                         } else {
                             String path = file.toFile().getAbsolutePath().replace(Folder.getAbsolutePath(), "");
-                            Bukkit.getLogger().info(path);
 
-                            if (path.contains("session.lock") || path.contains("LocalBackup")) {
-                                Logger.LogInfo("Skipping Found Blacklisted File");
-                            } else {
-                                File newfile = new File(OutputFolder + File.separator + path);
-                                FileUtils.copyDirectory(file.toFile(), newfile);
+                            for (String BlackListedName: BlackListedNames) {
+                                if (path.contains(BlackListedName)) {
+                                    Logger.LogInfo("Skipping Found Blacklisted File");
+                                } else {
+                                    File newfile = new File(OutputFolder + File.separator + path);
+                                    FileUtils.copyDirectory(file.toFile(), newfile);
+                                }
                             }
                         }
 
@@ -137,9 +139,9 @@ public class Backup {
                 throw new RuntimeException(e);
             }
 
-            Bukkit.getServer().spigot().restart();
-
             Logger.LogInfo("&aCopy Completed! Duration: " + (System.nanoTime()-start));
+
+            Bukkit.getServer().spigot().restart();
 
         });
     }
